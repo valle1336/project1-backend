6da6c69488ee244949152ca03ccde9586ab85bd1
valle1336 <7alexxi@gmail.com>
2024-05-21 15:32:52 +0200

    API is now completed users can now send messages!

--- src/main/java/com/valle1336/demo/Controllers/MessageController.java
+++ src/main/java/com/valle1336/demo/Controllers/MessageController.java
@@ -1,16 +1,38 @@
 package com.valle1336.demo.Controllers;
 
 import com.valle1336.demo.Entities.MessageEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.valle1336.demo.Repositories.MessageRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class MessageController {
-    @PostMapping("/sendMessage")
-    public String sendMessage(@RequestBody MessageEntity message) {
+    private final MessageRepository messageRepository;
 
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @PostMapping("/sendMessage")
+    public ResponseEntity<MessageEntity> sendMessage(@RequestBody MessageEntity message) {
+        MessageEntity newMessage = new MessageEntity();
+        newMessage.setMessage(message.getMessage());
+        newMessage.setMail(message.getMail());
+        newMessage.setName(message.getName());
+        newMessage.setPhoneNumber(message.getPhoneNumber());
+        newMessage.setCreationDate(LocalDateTime.now());
+
+        MessageEntity saveMessage = messageRepository.save(newMessage);
+
+        return new ResponseEntity<MessageEntity>(saveMessage, HttpStatus.OK);
+    }
+    @GetMapping("/getAll")
+    public List<MessageEntity> allMessages() {
+        return messageRepository.findAll();
     }
 }
